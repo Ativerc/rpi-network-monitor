a0c1d318cb5ad0aa504d188b153e780b8969ac77
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2021-05-20 22:38:50 +0530

    Added comments for Status and Activity LEDs.

--- led_pinouts.py
+++ led_pinouts.py
@@ -1,9 +1,12 @@
 import board
 import digitalio
 
-yellowled = digitalio.DigitalInOut(board.D18)
+# Status LED
 redled = digitalio.DigitalInOut(board.D17)
 greenled = digitalio.DigitalInOut(board.D27)
 blueled = digitalio.DigitalInOut(board.D26)
+
+# Activity LED 
+yellowled = digitalio.DigitalInOut(board.D18)
 
 # How do i print these pinouts?

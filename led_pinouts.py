9d004023af374428f65b28641927104b44cc836e
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2021-08-09 18:04:34 +0530

    Added pinout_dirty_printer() to led_pinouts.py
    pinout_dirty_printer() is really bad.
    A shortcut to just print the pinouts.
    
    See the code to see why its dirty.
    
    MUST change later.

--- led_pinouts.py
+++ led_pinouts.py
@@ -1,9 +1,22 @@
 import board
 import digitalio
 
+yellowled_pin = 18
+redled_pin = 17
+greenled_pin = 27
+blueled_pin = 26
+
 yellowled = digitalio.DigitalInOut(board.D18)
 redled = digitalio.DigitalInOut(board.D17)
 greenled = digitalio.DigitalInOut(board.D27)
 blueled = digitalio.DigitalInOut(board.D26)
 
 # How do i print these pinouts?
+
+
+def dirty_pinout_printer():
+	print(f"STATUS LED PIN: {yellowled_pin}")
+	print(f"RED LED PIN: {redled_pin}")
+	print(f"GREEN LED PIN: {greenled_pin}")
+	print(f"BLUE LED PIN: {blueled_pin}")
+

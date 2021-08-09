2e005fbed38e3a85db7a3a00c92c51ad6e1ddc06
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2021-08-10 01:49:14 +0530

    pinout_printer() fixes the shortcomings with dirty_pinout_printer(). Yay!

--- led_pinouts.py
+++ led_pinouts.py
@@ -1,10 +1,6 @@
 import board
 import digitalio
 
-yellowled_pin = 18
-redled_pin = 17
-greenled_pin = 27
-blueled_pin = 26
 
 # Activity LED
 yellowled = digitalio.DigitalInOut(board.D18)
@@ -14,16 +10,10 @@ redled = digitalio.DigitalInOut(board.D17)
 greenled = digitalio.DigitalInOut(board.D27)
 blueled = digitalio.DigitalInOut(board.D26)
 
-# How do i print these pinouts?
 
+def pinout_printer():
+	print(f"ACTIVITY LED PIN: {yellowled._pin}")
+	print(f"RED LED PIN: {redled._pin}")
+	print(f"GREEN LED PIN: {greenled._pin}")
+	print(f"BLUE LED PIN: {blueled._pin}")
 
-def dirty_pinout_printer():
-	print(f"ACTIVITY LED PIN: {yellowled_pin}")
-	print(f"RED LED PIN: {redled_pin}")
-	print(f"GREEN LED PIN: {greenled_pin}")
-	print(f"BLUE LED PIN: {blueled_pin}")
-
-
-# dirty_pinout_printer() sucks. 
-# I need a way to print the value of the pin from the value of 
-# the pins from redled, greenled, etc. variables.
